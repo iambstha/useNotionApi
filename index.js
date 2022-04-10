c6a31101d693585d7a75d36b86ab66a856e86f64
Bishal Shrestha <iambstha@gmail.com>
2022-04-10 19:31:02 +0545

    Copied version

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-const dotenv = require('dotenv').config()
+const express = require('express')
+const getVideos = require('./services/notion')
+const PORT = process.env.PORT || 5000
 
-const { Client } = require('@notionhq/client')
+const app = express()
 
-const notion = new Client ({
-    auth: process.env.NOTION_KEY
+app.use(express.static('public'))
+
+app.get('/videos', async (req, res) => {
+  const videos = await getVideos()
+  res.json(videos)
 })
 
-const databseID = process.env.NOTION_DATABASE_ID
-
-async function getData(){
-    const payload = {
-        path : `databases/${databseID}/query`,
-        method: 'POST'
-    }
-
-    const { results } = await notion.request(payload)
-
-    const videos = results.map((page) => {
-      return {
-        id: page.id,
-        title: page.properties.Name.title[0].text.content,
-        date: page.properties.Date.date.start,
-        tags: page.properties.Tags.rich_text[0].text.content,
-        description: page.properties.Description.rich_text[0].text.content,
-      }
-    })
-
-    console.log(videos)
-}
-
-getData()
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
