d7798b0ebe965ec34f375b8d9c3a1b87c716a2b0
Bishal Shrestha <iambstha@gmail.com>
2022-04-10 18:54:49 +0545

    First workign prototype

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello")
+const dotenv = require('dotenv').config()
+
+const { Client } = require('@notionhq/client')
+
+const notion = new Client ({
+    auth: process.env.NOTION_KEY
+})
+
+const databseID = process.env.NOTION_DATABASE_ID
+
+async function getData(){
+    const payload = {
+        path : `databases/${databseID}/query`,
+        method: 'POST'
+    }
+
+    const { results } = await notion.request(payload)
+
+    const videos = results.map((page) => {
+      return {
+        id: page.id,
+        title: page.properties.Name.title[0].text.content,
+        date: page.properties.Date.date.start,
+        tags: page.properties.Tags.rich_text[0].text.content,
+        description: page.properties.Description.rich_text[0].text.content,
+      }
+    })
+
+    console.log(videos)
+}
+
+getData()
